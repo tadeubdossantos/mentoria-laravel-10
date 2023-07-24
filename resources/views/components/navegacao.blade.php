783929a7e9a1287423191f5d7b33a2c45283f5dc
Tadeu Bispo <tadeub.dossantos@gmail.com>
2023-07-24 14:04:08 -0300

    front

---
+++ resources/views/components/navegacao.blade.php
@@ -0,0 +1,26 @@
+<ul class="nav flex-column">
+    <li class="nav-item">
+        <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
+        <svg class="bi"><use xlink:href="#house-fill"/></svg>
+        Dashboard
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link d-flex align-items-center gap-2" href="#">
+        <svg class="bi"><use xlink:href="#file-earmark"/></svg>
+        Venda
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link d-flex align-items-center gap-2" href="#">
+        <svg class="bi"><use xlink:href="#cart"/></svg>
+        Produto
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link d-flex align-items-center gap-2" href="#">
+        <svg class="bi"><use xlink:href="#people"/></svg>
+        Clientes
+        </a>
+    </li>
+</ul>
